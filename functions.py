a904dcea9a0998234d7937e5e8ff54fa2f73090d
Enes Çevik <enescevik@Eness-MBP.attlocal.net>
2024-08-06 06:57:30 -0400

    Added features of adding or removing support or resistance and finding the support and resistance around a value

--- functions.py
+++ functions.py
@@ -1,14 +1,24 @@
 class SupportResistanceTradingBot():
-    def __init__(self, entry_price: float):
-        self.entry_price = entry_price
-        self.price = []
-        self.position = None
+    def __init__(self, list_of_lines: list):
+        self.list_of_lines = sorted(list_of_lines)
 
-    def update_price(self, price: float):
-        """Update the bot with the latest price."""
-        self.prices.append(price)
-        if len(self.prices) > 5:
-            self.prices.pop(0)  # Maintain only the necessary history
+    def add_support_or_resistance(self, value: float):
+        self.list_of_lines.append(value)
+        self.list_of_lines.sort()
 
-    #def compare_price_and_lines(self, )
+    def remove_support_or_resistance(self, value: float):
+        self.list_of_lines.remove(value)
+
+    def compare_price_and_lines(self, price):
+        temp_list = self.list_of_lines + [price]
+        temp_list.sort()
+        ind = temp_list.index(price)
+        return temp_list[ind - 1], temp_list[ind + 1]
+
+first_model = SupportResistanceTradingBot([5, 10, 20, 40])
+first_model.add_support_or_resistance(15.5)
+first_model.remove_support_or_resistance(10)
+lower, upper = first_model.compare_price_and_lines(17)
+
+print(lower, upper)
 
