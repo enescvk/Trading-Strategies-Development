9d962d79d21d0a14a905b7ba5d1dddcbac00b982
Enes Çevik <enescevik@Eness-MBP.attlocal.net>
2024-08-10 20:22:39 -0400

    Finding closest support and resistance lines feature added

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 class SupportResistanceTradingBot():
-    def __init__(self, df, list_of_lines: list, wallet: float):
-        self.list_of_lines = sorted(list_of_lines)
+    def __init__(self, df, CP_list: list, wallet: float):
+        self.CP_list = sorted(CP_list)
         self.wallet = wallet
         self.df = df
         self.position_status = None
@@ -12,31 +12,44 @@ class SupportResistanceTradingBot():
         self.quantity = 0
         self.transaction_log = []
 
+    def create_buy_and_sell_cps(self, x):
+        self.buy_cps = [i * (100 + x)/100 for i in self.CP_list]
+        self.sell_cps = [i * (100 - x)/100 for i in self.CP_list]
+        return self.buy_cps, self.sell_cps
+
     def update_prices(self, price_cur, price_prev): # NEW
         # This function updates the price with the current one.
         self.current_price = price_cur
         self.previous_price = price_prev
 
-    def add_support_or_resistance(self, value: float):
+    def add_cl(self, value: float):
         # This function adds another support or resistance level.
-        self.list_of_lines.append(value)
-        self.list_of_lines.sort()
+        self.CP_list.append(value)
+        self.CP_list.sort()
 
-    def remove_support_or_resistance(self, value: float):
+    def remove_cl(self, value):
         # This function removes a support or resistance level.
-        self.list_of_lines.remove(value)
+        self.CP_list.remove(value)
 
-    def check_price_and_lines(self):
+    def get_sup_and_res_lines(self, x):
         # This function checks price and finds the closest support and resistance level.
-        temp_list = self.list_of_lines + [self.current_price]
-        temp_list.sort()
-        ind = temp_list.index(self.current_price)
-        if ind != 0:
-            return temp_list[ind - 1], temp_list[ind + 1]
+        temp_buy_list, temp_sell_list = self.create_buy_and_sell_cps(x)
+
+        temp_buy_list = temp_buy_list + [self.current_price]
+        temp_buy_list.sort()
+        ind_buy = temp_buy_list.index(self.current_price)
+
+        temp_sell_list = temp_sell_list + [self.current_price]
+        temp_sell_list.sort()
+        ind_sell = temp_sell_list.index(self.current_price)
+
+        if ind_buy != 0 or ind_sell != len(temp_sell_list) - 1:
+            return temp_buy_list[ind_buy - 1], temp_sell_list[ind_sell + 1]
         else:
-            raise ValueError("There is no lower support level than the price! You have to add a lower support level!")
+            raise ValueError("There is no lower support level or higher resistance level than the price!")
         
     def log_transaction(self, transaction_type, position_type, price, quantity): # NEW
+        # This function logs necessary information whenever there is a new transaction
         transaction = {
             'type': transaction_type,
             'position_type': position_type,
@@ -48,7 +61,6 @@ class SupportResistanceTradingBot():
         self.transaction_log.append(transaction)
 
     def open_long_pos(self): # NEW
-        #lower, upper = self.check_price_and_lines(self.current_price)
         if (self.position_status is None) and (self.wallet > 0):
             # Buying logic
             self.position_status = "long"
@@ -59,7 +71,6 @@ class SupportResistanceTradingBot():
             self.log_transaction("buy", self.position_status, self.current_price, self.quantity)
 
     def close_long_pos(self): # NEW
-        #lower, upper = self.check_price_and_lines(self.current_price)
         if self.position_status == "long":
             self.wallet += self.quantity * self.current_price
             self.log_transaction("sell", self.position_status, self.current_price, self.quantity)
@@ -69,7 +80,6 @@ class SupportResistanceTradingBot():
 
     def open_short_pos(self): # NEW
         # Implement short selling logic and log the transaction
-        #lower, upper = self.check_price_and_lines(self.current_price)
         if (self.position_status is None):
             # Buying logic
             self.position_status = "short"
@@ -81,7 +91,6 @@ class SupportResistanceTradingBot():
 
     def close_short_pos(self): # NEW
         # Implement closing short position logic and log the transaction
-        #lower, upper = self.check_price_and_lines(self.current_price)
         if self.position_status == "short":
             self.wallet += -1 * self.quantity * self.current_price
             self.log_transaction("sell", self.position_status, self.current_price, self.quantity)
@@ -91,9 +100,13 @@ class SupportResistanceTradingBot():
 
             
 
-first_model = SupportResistanceTradingBot([5, 10, 20, 40])
-first_model.add_support_or_resistance(15.5)
-first_model.remove_support_or_resistance(10)
-lower, upper = first_model.check_price_and_lines(4)
-
-print(lower, upper)
+first_model = SupportResistanceTradingBot(None, [5, 10, 20, 40], 1000)
+buy_lines, sell_lines = first_model.create_buy_and_sell_cps(5)
+print(f"Buy Lines: {buy_lines}\nSell Lines: {sell_lines}")
+first_model.add_cl(15.0)
+first_model.remove_cl(10.0)
+buy_lines, sell_lines = first_model.create_buy_and_sell_cps(5)
+print(f"New Buy Lines: {buy_lines}\nNew Sell Lines: {sell_lines}")
+first_model.update_prices(30, 33)
+support, resistance = first_model.get_sup_and_res_lines(5)
+print(f"Support: {support}\nResistance: {resistance}")
---
+++ trials.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+list = [10, 80, 100, 120, 150]
+length = len(list)
+ind = list.index(150)
+
+print(length, ind)
